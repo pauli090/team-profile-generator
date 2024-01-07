12a1deafa8eea16a06feea97a172c1ebc7123ec7
pauli090 <paulinkaa090@gmail.com>
2024-01-07 18:20:58 +0000

    reinstalled node modules, fixed error with inquirer, added function in index.js to add a manager

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -12,4 +13,41 @@ const render = require("./src/page-template.js");
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+//name id email officeNumber
+function addManager() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "manName",
+                message: "Enter the team manager's name",
+            },
+            {
+                type: "input",
+                name: "manId",
+                message: "Enter the team manager's id",
+            },
+            {
+                type: "input",
+                name: "manEmail",
+                message: "Enter the team manager's email",
+            },
+            {
+                type: "input",
+                name: "manOfficeNumber",
+                message: "Enter the team manager's office number",
+            },
+        ])
+        .then((data) => {
+            const manager = new Manager (
+            data.manName,
+            data.manId,
+            data.manEmail,
+            data.manOfficeNumber
+            );
+            addMember();
+        }
+  )
+}
 
+addManager();
